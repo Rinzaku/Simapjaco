ce7f888d8eff0096800d5c38916c76ac6c33abe5
gilberto <gilberto_aquarius@hotmail.com>
2015-11-22 09:52:11 -0600

    agregada tabla para cambio producto

--- src/controllers/Cambio.java
+++ src/controllers/Cambio.java
@@ -9,33 +9,62 @@ import instancias.*;
 
 public class Cambio {
 	
+	private Ventas_model vmodel; 
+	private Detalle_Venta_model dvmodel;
+	private Ropa_model rmodel;
+	private Modelo_model mmodel;
+	private Ventas ventas;
+	private ArrayList<Detalle_Venta> detalles;
+	
 	public String obten_venta(int id_venta){
-		Ventas_model vmodel = new Ventas_model();
-		Detalle_Venta_model dvmodel = new Detalle_Venta_model();
-		Ropa_model rmodel = new Ropa_model();
-		Modelo_model mmodel = new Modelo_model();
+		vmodel = new Ventas_model();
+		dvmodel = new Detalle_Venta_model();
+		rmodel = new Ropa_model();
+		mmodel = new Modelo_model();
 		
-		Ventas ventas = vmodel.find_venta(id_venta);
-		ArrayList<Detalle_Venta> detalles = dvmodel.find_detalle_venta(id_venta);
+		ventas = vmodel.find_venta(id_venta);
+		detalles = dvmodel.find_detalle_venta(id_venta);
 		String descripcion = "";
 		
-		descripcion = ventas.getId_venta()+" \t "+ventas.getFecha()+" \t\t "+ventas.getNo_articulos()+" \t\t\t "+ventas.getTotal_venta()+" \n";
-		descripcion += "\nModelo \t Descripción \t\t\t\t No. artículos \t Precio unitario \t Estado \n";
-		for (Detalle_Venta detalle_venta : detalles) {
-			String desc = rmodel.find_ropa(detalle_venta.getId_ropa()).getDescricion();
-			if(desc.length()>=20){
-				descripcion += mmodel.find_modelo(detalle_venta.getId_modelo()).getModelo()+" \t "+desc+" \t "
-						+ "" + detalle_venta.getCantidad_articulos()+" \t\t\t " + detalle_venta.getPrecio_unitario() + " \t\t " + detalle_venta.getEstado() +"\n";
-			}else{
-				descripcion += mmodel.find_modelo(detalle_venta.getId_modelo()).getModelo()+" \t "+desc+" \t\t\t "
-						+ "" + detalle_venta.getCantidad_articulos()+" \t\t\t " + detalle_venta.getPrecio_unitario() + " \t\t " + detalle_venta.getEstado() +"\n";
-			}
-			
-			System.out.println("Cadena: "+rmodel.find_ropa(detalle_venta.getId_ropa()).getDescricion().length());
-		}
+		descripcion = ventas.getId_venta()+" \t "+ventas.getFecha()+" \t "+ventas.getNo_articulos()+" \t\t "+ventas.getTotal_venta()+" \n";
+//		descripcion += "\nModelo \t Descripción \t\t\t\t No. artículos \t Precio unitario \t Estado \n";
+//		for (Detalle_Venta detalle_venta : detalles) {
+//			String desc = rmodel.find_ropa(detalle_venta.getId_ropa()).getDescricion();
+//			if (desc.length()<=15) {
+//				descripcion += mmodel.find_modelo(detalle_venta.getId_modelo()).getModelo()+" \t "+desc+" \t\t\t "
+//						+ "" + detalle_venta.getCantidad_articulos()+" \t\t\t " + detalle_venta.getPrecio_unitario() + " \t\t " + detalle_venta.getEstado() +"\n";
+//			}else if(desc.length()<=25){
+//				descripcion += mmodel.find_modelo(detalle_venta.getId_modelo()).getModelo()+" \t "+desc+" \t\t "
+//						+ "" + detalle_venta.getCantidad_articulos()+" \t\t\t " + detalle_venta.getPrecio_unitario() + " \t\t " + detalle_venta.getEstado() +"\n";
+//			}else{
+//				descripcion += mmodel.find_modelo(detalle_venta.getId_modelo()).getModelo()+" \t "+desc+" \t "
+//						+ "" + detalle_venta.getCantidad_articulos()+" \t\t\t " + detalle_venta.getPrecio_unitario() + " \t\t " + detalle_venta.getEstado() +"\n";
+//			}
+//			
+//			System.out.println("Cadena: "+rmodel.find_ropa(detalle_venta.getId_ropa()).getDescricion().length());
+//		}
 		return descripcion;
 	}
 	
+	public String[][] obten_detalles_venta(int id_venta){
+		String[][] datos = new String[detalles.size()][];
+		int i=0;
+		for (Detalle_Venta detalle : detalles) {
+			String[] dato = new String[5];
+			dato[0] = mmodel.find_modelo(detalle.getId_modelo()).getModelo();
+			dato[1] = rmodel.find_ropa(detalle.getId_ropa()).getDescricion();
+			dato[2] = ""+detalle.getCantidad_articulos();
+			dato[3] = ""+detalle.getPrecio_unitario();
+			dato[4] = detalle.getEstado();
+			datos[i] = dato;
+			i++;
+			
+		}
+		return datos;
+	}
+//	public String obten_precio(){
+//		
+//	}
 	public String fecha(){
 		Calendar calendario = new GregorianCalendar();
 		String day = Integer.toString(calendario.get(Calendar.DATE));
--- src/vistas/CambiarProducto.java
+++ src/vistas/CambiarProducto.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+
+
+
+
+
 
 
 
@@ -24,6 +30,9 @@ import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
 
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.ScrollPane;
 import java.awt.Toolkit;
 import java.awt.Font;
 
@@ -38,17 +47,24 @@ import javax.swing.DropMode;
 
 import controllers.Cambio;
 
+import javax.swing.JTable;
+
 public class CambiarProducto extends JFrame {
-	private JTextField textModeloVenta;
 	private JTextField textModeloCambio;
-	private JTextField textPrecioVenta;
 	private JTextField textPrecioCambio;
 	private JTextField textFolio;
 	private JTextField textDiferencia;
-	private JTextArea txtrdescripcionDeLa;
+	private JTextArea textAreaDatosVenta;
 	
 	private Cambio cambios;
+	private JTable tableCambios;
+	private DefaultTableModel modelCambios;
+	private JScrollPane scrollCambios;
 
+	private String [] cabecera = {"Modelo", "Descripcion","No. articulos", "Precio unitario", "Estado"};
+	private String [][] datostable;
+	private boolean bandera=false;
+	
 	/**
 	 * Create the frame.
 	 */
@@ -61,17 +77,39 @@ public class CambiarProducto extends JFrame {
 		
 		setTitle("Cambio de Producto");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 800, 350);
+		setBounds(100, 100, 810, 380);
 		
 		textFolio = new JTextField();
+		textFolio.setHorizontalAlignment(SwingConstants.RIGHT);
 		textFolio.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyTyped(KeyEvent key) {
 				if (key.getKeyChar()=='\n') {
 					if (!textFolio.getText().trim().isEmpty()) {
+						if (bandera) {
+							getContentPane().remove(scrollCambios);
+						} 
+						
 						int folio = Integer.parseInt(textFolio.getText());
 						String datos = cambios.obten_venta(folio);
-						txtrdescripcionDeLa.setText("Folio \t Fecha \t\t Total artículos \t Total Venta \n"+datos);
+						textAreaDatosVenta.setText("Folio \t Fecha \t\t Total artículos \t Total Venta \n"+datos);
+						datostable = cambios.obten_detalles_venta(folio);
+						modelCambios = new DefaultTableModel(datostable,cabecera){
+							@Override
+							public boolean isCellEditable(int row, int col){
+								return false;
+							}
+						};
+						tableCambios = new JTable(modelCambios);
+						tableCambios.setBackground(new Color(176, 224, 230));
+						scrollCambios = new JScrollPane(tableCambios);
+						scrollCambios.setPreferredSize(new Dimension(400, 150));
+						System.out.println(bandera);
+						getContentPane().add(scrollCambios,"cell 0 2 9 12,grow");
+						
+						scrollCambios.setVisible(true);
+						bandera = true;
+						
 					} else {
 						JOptionPane.showMessageDialog(null, "Ingresa un folio de venta");
 					}
@@ -79,7 +117,7 @@ public class CambiarProducto extends JFrame {
 				}
 			}
 		});
-		getContentPane().setLayout(new MigLayout("", "[144px][18px][149px][32px][46px][4px][101px][33px][163px]", "[21px][115px][20px][21px][21px][][][][][][baseline][41px,bottom]"));
+		getContentPane().setLayout(new MigLayout("", "[144px,grow][18px][149px][32px][46px][4px][101px][33px][163px]", "[21px][60.00px,grow][20px][21px][21px][0.00][0.00][0.00][0.00][0.00][][][0.00][][][][][][][baseline][41px,bottom]"));
 		
 		JLabel lblFolio = new JLabel("Folio :");
 		lblFolio.setForeground(Color.WHITE);
@@ -93,64 +131,47 @@ public class CambiarProducto extends JFrame {
 		lblFecha.setFont(new Font("Tahoma", Font.BOLD, 15));
 		getContentPane().add(lblFecha, "cell 8 0,alignx right,aligny top");
 		
-		txtrdescripcionDeLa = new JTextArea();
-		txtrdescripcionDeLa.setEditable(false);
-		txtrdescripcionDeLa.setFont(new Font("Monospaced", Font.BOLD | Font.ITALIC, 13));
-		txtrdescripcionDeLa.setText("*******************************Descripcion de la venta*******************************");
-		txtrdescripcionDeLa.setTabSize(7);
-		getContentPane().add(txtrdescripcionDeLa, "cell 0 1 9 1,grow");
+		textAreaDatosVenta = new JTextArea();
+		textAreaDatosVenta.setFont(new Font("Tahoma",Font.BOLD+Font.ITALIC, 13));
+		textAreaDatosVenta.setText("***************************************Descripcion de la venta*************************************");
+		getContentPane().add(textAreaDatosVenta, "cell 0 1 9 1,grow");
 		
-		JLabel lblModelo = new JLabel("Modelo de  Venta:");
-		lblModelo.setForeground(Color.WHITE);
-		lblModelo.setFont(new Font("Tahoma", Font.BOLD, 15));
-		getContentPane().add(lblModelo, "cell 0 2,growx,aligny bottom");
-		
-		textModeloVenta = new JTextField();
-		getContentPane().add(textModeloVenta, "cell 2 2,growx,aligny top");
-		textModeloVenta.setColumns(10);
-		
-		textModeloCambio = new JTextField();
-		getContentPane().add(textModeloCambio, "cell 2 3,growx,aligny top");
-		textModeloCambio.setColumns(10);
+//		tableCambios = new JTable();
+//		getContentPane().add(tableCambios, "cell 0 2 9 12,grow");
 		
 		JLabel lblModeloCambio = new JLabel("Modelo de Cambio :");
 		lblModeloCambio.setForeground(Color.WHITE);
 		lblModeloCambio.setFont(new Font("Tahoma", Font.BOLD, 15));
-		getContentPane().add(lblModeloCambio, "cell 0 3,alignx left,aligny center");
+		getContentPane().add(lblModeloCambio, "cell 0 15,alignx left,aligny center");
 		
-		JLabel lblPrecioVenta = new JLabel("Precio :");
-		lblPrecioVenta.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblPrecioVenta.setForeground(Color.WHITE);
-		getContentPane().add(lblPrecioVenta, "cell 6 2,growx,aligny bottom");
-		
-		textPrecioVenta = new JTextField();
-		getContentPane().add(textPrecioVenta, "cell 8 2,growx,aligny top");
-		textPrecioVenta.setColumns(10);
+		textModeloCambio = new JTextField();
+		getContentPane().add(textModeloCambio, "cell 2 15,growx,aligny top");
+		textModeloCambio.setColumns(10);
 		
 		JLabel lblPrecioCambio = new JLabel("Precio :");
 		lblPrecioCambio.setForeground(Color.WHITE);
 		lblPrecioCambio.setFont(new Font("Tahoma", Font.BOLD, 15));
-		getContentPane().add(lblPrecioCambio, "cell 6 3,alignx left,aligny top");
+		getContentPane().add(lblPrecioCambio, "cell 6 15,alignx left,aligny top");
 		
 		textPrecioCambio = new JTextField();
-		getContentPane().add(textPrecioCambio, "cell 8 3,growx,aligny bottom");
+		getContentPane().add(textPrecioCambio, "cell 8 15,growx,aligny bottom");
 		textPrecioCambio.setColumns(10);
 		
 		JLabel lblDiferencia = new JLabel("Diferencia :");
 		lblDiferencia.setForeground(Color.WHITE);
 		lblDiferencia.setFont(new Font("Tahoma", Font.BOLD, 15));
-		getContentPane().add(lblDiferencia, "cell 6 4,alignx left,aligny top");
+		getContentPane().add(lblDiferencia, "cell 6 17,alignx left,aligny top");
 		
 		textDiferencia = new JTextField();
 		textDiferencia.setEditable(false);
-		getContentPane().add(textDiferencia, "cell 8 4,growx,aligny bottom");
+		getContentPane().add(textDiferencia, "cell 8 17,growx,aligny bottom");
 		textDiferencia.setColumns(10);
 		
 		JButton btnAceptar = new JButton("");
 		btnAceptar.setBackground(new Color(51, 0, 204));
 		btnAceptar.setIcon(new ImageIcon(CambiarProducto.class.getResource("/imagenes/ok32.png")));
 		
-		getContentPane().add(btnAceptar, "cell 0 11 3 1,growx,aligny bottom");
+		getContentPane().add(btnAceptar, "cell 0 20 3 1,growx,aligny bottom");
 		
 		JButton btnCancelar = new JButton("");
 		btnCancelar.setBackground(new Color(51, 0, 204));
@@ -159,7 +180,7 @@ public class CambiarProducto extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		getContentPane().add(btnCancelar, "cell 4 11 5 1,growx,aligny bottom");
+		getContentPane().add(btnCancelar, "cell 4 20 5 1,growx,aligny bottom");
 	}
 }
 
