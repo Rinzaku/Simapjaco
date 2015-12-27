f6c33c94753e4bcc19f2f35a1a3e9fb28fcfe95b
LcHer <ceciferch@gmail.com>
2015-12-26 22:30:46 -0600

    ALta empleados

--- src/controllers/Empleados_c.java
+++ src/controllers/Empleados_c.java
@@ -1,5 +1,33 @@
 package controllers;
 
-public class Empleados_c {
+import models.Empleados_model;
+import instancias.Empleado;
 
+public class Empleados_c {
+	Empleado empleadoN;
+	Empleados_model modelEmpleado;
+	
+	public int insertEmpleado(String nombre,String Apellido,String direccion,String tel,String fecha){
+		int clave=0;
+		empleadoN=new Empleado();
+		modelEmpleado=new Empleados_model();
+		empleadoN.setNombre(nombre);
+		empleadoN.setApellidos(Apellido);
+		empleadoN.setDireccion(direccion);
+		empleadoN.setTelefono(tel);
+		empleadoN.setFecha_inicio(fecha);
+		
+		clave=modelEmpleado.insert_empleado(empleadoN);
+		if (clave>-1) {
+			System.out.println("la clave del if es :"+clave);
+			return clave;
+		}
+		else{
+			System.out.println("else no se efectuo la insercion");
+			return -1;
+		}
+		
+		
+	}
+	
 }
--- src/vistas/Empleados.java
+++ src/vistas/Empleados.java
@@ -6,19 +6,32 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import java.awt.Color;
+
 import javax.swing.JLabel;
+
 import java.awt.Font;
+
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
+
 import com.toedter.components.JLocaleChooser;
 import com.toedter.calendar.JDateChooser;
+
+import controllers.Empleados_c;
+
 import javax.swing.JButton;
+
+import models.Empleados_model;
 import net.miginfocom.swing.MigLayout;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.Arrays;
 
 public class Empleados extends JFrame {
 
@@ -26,6 +39,10 @@ public class Empleados extends JFrame {
 	private JTextField textFieldNombre;
 	private JTextField textFieldApellido;
 	private JTextField textFieldTelefono;
+	private JButton buttonAlta;
+	private JTextArea txtDireccion;
+	private JDateChooser Calendario ;
+	private Empleados_c controlEmpleado;
 
 	/**
 	 * Launch the application.
@@ -47,6 +64,7 @@ public class Empleados extends JFrame {
 	 * Create the frame.
 	 */
 	public Empleados() {
+		controlEmpleado=new Empleados_c();
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 657, 418);
@@ -84,7 +102,7 @@ public class Empleados extends JFrame {
 		lblDireccion.setForeground(Color.WHITE);
 		contentPane.add(lblDireccion);
 		
-		JTextArea txtDireccion = new JTextArea();
+		txtDireccion = new JTextArea();
 		txtDireccion.setBounds(107, 240, 186, 48);
 		contentPane.add(txtDireccion);
 		
@@ -111,7 +129,7 @@ public class Empleados extends JFrame {
 		lblFecha.setForeground(Color.WHITE);
 		contentPane.add(lblFecha);
 		
-		JDateChooser Calendario = new JDateChooser();
+		Calendario = new JDateChooser();
 		Calendario.setBounds(107, 188, 186, 20);
 		contentPane.add(Calendario);
 		
@@ -120,7 +138,8 @@ public class Empleados extends JFrame {
 		buttonBuscar.setIcon(new ImageIcon(Empleados.class.getResource("/imagenes/search48.png")));
 		contentPane.add(buttonBuscar);
 		
-		JButton buttonAlta = new JButton("");
+	    buttonAlta = new JButton("");
+	    
 		buttonAlta.setBounds(229, 311, 187, 57);
 		buttonAlta.setIcon(new ImageIcon(Empleados.class.getResource("/imagenes/ok48.png")));
 		contentPane.add(buttonAlta);
@@ -129,5 +148,73 @@ public class Empleados extends JFrame {
 		buttonBaja.setBounds(442, 311, 187, 57);
 		buttonBaja.setIcon(new ImageIcon(Empleados.class.getResource("/imagenes/error48.png")));
 		contentPane.add(buttonBaja);
+		
+		buttonAlta.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent arg0) {
+
+	    		if(!textFieldNombre.getText().isEmpty() && !textFieldApellido.getText().isEmpty() && !textFieldTelefono.getText().isEmpty() && !txtDireccion.getText().isEmpty()){
+	    			int clave=controlEmpleado.insertEmpleado(textFieldNombre.getText(),textFieldApellido.getText(),txtDireccion.getText(),textFieldTelefono.getText(),Calendario.getDateFormatString());  
+//	    			if (!formatoTel(textFieldTelefono.getText())) {
+//	    				JOptionPane.showMessageDialog(null, "El formato del telefono es incorrecto ");
+//
+//
+//	    			}else{
+//	    				JOptionPane.showMessageDialog(null, "El identificador del empleado  "+textFieldNombre.getText()+" "+textFieldApellido.getText()+"  es :"+"\n"+""+clave);
+//	    				limpiaCampos();
+//	    			}
+	    			if (clave>-1) {
+						JOptionPane.showMessageDialog(null, "empleado registrado con el identificador :"+clave);
+					}else{
+						JOptionPane.showMessageDialog(null, "Error al insertar empleado");
+					}
+
+	    		}else{
+	    			JOptionPane.showMessageDialog(null, "Llena todos los campos");
+	    		}
+	    	}
+	    });
+	
 	}
-}
+	
+	
+	public void limpiaCampos(){
+		textFieldNombre.setText("");
+		textFieldApellido.setText("");
+		textFieldTelefono.setText("");
+		txtDireccion.setText("");
+		Calendario.setDateFormatString("");
+			
+	}
+	
+	public boolean formatoTel(String telefono){
+		char []numero= telefono.toCharArray();
+		boolean bandera=false;
+		System.out.println(Arrays.toString(numero)+"tamaño  :"+numero.length);
+		
+		if (numero.length ==8 || numero.length==10) {
+			for (int i = 0; i < numero.length; i++) {
+				if (numero[i]==1 || numero[i]==2 || numero[i]==3 ||numero[i]== 4 || numero[i]==5 || numero[i]==6 || numero[i]==7 || numero[i]==8 || numero[i]==9 || numero[i]==0) {
+					System.out.println("true"+i);
+					bandera=true;
+				}
+				else
+					bandera= false;
+			}
+
+		}
+		else{
+			bandera =false;
+		}
+		return bandera;
+	}
+
+	
+	
+}//fin clase
+
+
+
+
+
+
+
