64183c30b1205c6c248b0136d33493d7f10abdf7
Gilberto <rinzaku04@gmail.com>
2016-01-21 22:51:20 -0600

    update

--- src/vistas/Run.java
+++ src/vistas/Run.java
@@ -1,22 +1,24 @@
 package vistas;
 
-
+import java.awt.EventQueue;
 
 public class Run {
-
-
-
+	
 	public static void main(String[] args) {
 		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaSplash window =new VentanaSplash();
+					window.setBounds(0, 0, 600, 400);
+					window.setLocationRelativeTo(null);
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		
-		VentanaSplash window =new VentanaSplash();
-		window.setVisible(true);
-		window.setBounds(0, 0, 600, 400);
-		window.setLocationRelativeTo(null);
-		
-		
-
-
 	}
 
 }
